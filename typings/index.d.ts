b73c96141d25a1f51e1149538344155917b44085
otwm <otwmoo@gmail.com>
2017-01-20 20:11:05 +0900

    typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/antd/index.d.ts" />
+/// <reference path="globals/redux/index.d.ts" />
+/// <reference path="globals/reselect/index.d.ts" />
+/// <reference path="modules/debug/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
